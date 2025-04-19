6480a520df1819fbe7b1ad8636a3d83cc799aa3f
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-04-19 17:24:27 -0500

    Start working on OLAP goal

---
+++ scripts/olap_goal_product.py
@@ -0,0 +1,14 @@
+"""
+OLAP Goal Script
+File: scripts/olap_goal_product.py
+
+Goal: Analyze sales data from each product over time to find out which product sold the most in each region every week. 
+
+Action: This can help inform decision about supplying more products in particular regions or possibly changing pricing to raise or lower demand. 
+
+Process: 
+Divde sales amount by unit price of each product in each transaction to find the amount of each item sold.
+Group these transactions by week.
+Group these transactions by region. 
+
+"""
